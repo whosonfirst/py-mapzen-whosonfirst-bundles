4a75e6660f72e0d569e814fceb1689f1fe53b8e2
thisisaaronland <aaron@mapzen.com>
2016-01-26 00:35:29 +0000

    add a VERSION file; bump to 0.03

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.bundles.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+version = open("VERSION").read()
+desc = open("README.md").read()
 
 setup(
     name='mapzen.whosonfirst.bundles',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.bundles'],
-    version='0.02',
+    version=version,
     description='A package to generate Who\'s On First data bundles',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-bundles',
@@ -23,5 +35,5 @@ setup(
     scripts=[
         'scripts/wof-bundle-placetypes',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-bundle/releases/tag/v0.02',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-bundle/releases/tag/' + version,
     license='BSD')
