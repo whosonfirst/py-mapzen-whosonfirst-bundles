2ff6779be9180719e6457cb328333b12f9589fab
thisisaaronland <aaron@mapzen.com>
2017-03-21 22:08:03 +0000

    add requirements.txt; bump to 0.101

--- setup.py
+++ setup.py
@@ -26,10 +26,12 @@ setup(
     url='https://github.com/mapzen/py-mapzen-whosonfirst-bundles',
     install_requires=[
         'boto',
-        'mapzen.whosonfirst.placetypes>=0.22',
+        'mapzen.whosonfirst.placetypes>=0.21',
+        'mapzen.whosonfirst.placetypes.utils>=0.01',
         ],
     dependency_links=[
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master#egg=mapzen.whosonfirst.placetypes-0.22',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes/tarball/master#egg=mapzen.whosonfirst.placetypes-0.21',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-placetypes-utils/tarball/master#egg=mapzen.whosonfirst.placetypes.utils-0.01',
         ],
     packages=packages,
     scripts=[
